ad12f27fa68d2bd224a78682d7b793ed1a7ae8b7
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-05 22:53:33 -0500

    feat: added profile services implements

--- src/main/java/com/acme/center/platform/profiles/domain/model/aggregates/Profile.java
+++ src/main/java/com/acme/center/platform/profiles/domain/model/aggregates/Profile.java
@@ -1,5 +1,6 @@
 package com.acme.center.platform.profiles.domain.model.aggregates;
 
+import com.acme.center.platform.profiles.domain.model.commands.CreateProfileCommand;
 import com.acme.center.platform.profiles.domain.model.valueobjects.EmailAddress;
 import com.acme.center.platform.profiles.domain.model.valueobjects.PersonName;
 import com.acme.center.platform.profiles.domain.model.valueobjects.StreetAddress;
@@ -27,4 +28,17 @@ public class Profile extends AuditableAbstractAggregateRoot<Profile> {
             @AttributeOverride(name = "country", column = @Column(name = "street_address_country"))
     })
     private StreetAddress streetAddress;
+
+    public Profile(){}
+
+    public Profile(CreateProfileCommand command) {
+        this.name = new PersonName(command.firstName(), command.lastName());
+        this.emailAddress = new EmailAddress(command.email());
+        this.streetAddress = new StreetAddress(
+                command.street(),
+                command.number(),
+                command.city(),
+                command.postalCode(),
+                command.country());
+    }
 }
