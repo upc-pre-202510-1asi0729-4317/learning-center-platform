e8e0bffae8b3897e56b7c4f0977a791fe651ff1c
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-26 21:44:52 -0500

    feat(iam): add hashing service

---
+++ src/main/java/com/acme/center/platform/iam/infrastructure/hashing/bcrypt/sercices/HashingServiceImpl.java
@@ -0,0 +1,23 @@
+package com.acme.center.platform.iam.infrastructure.hashing.bcrypt.sercices;
+
+import com.acme.center.platform.iam.infrastructure.hashing.bcrypt.BCryptHashingService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class HashingServiceImpl implements BCryptHashingService {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    HashingServiceImpl() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
