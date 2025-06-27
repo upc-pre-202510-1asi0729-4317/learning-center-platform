354eb8884ffb03f443fe576c324ae04b7595847c
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-26 21:28:49 -0500

    feat(iam): add services interfaces

---
+++ src/main/java/com/acme/center/platform/iam/domain/services/RoleQueryService.java
@@ -0,0 +1,4 @@
+package com.acme.center.platform.iam.domain.services;
+
+public interface RoleQueryService {
+}
---
+++ src/main/java/com/acme/center/platform/iam/domain/services/UserCommandService.java
@@ -0,0 +1,16 @@
+package com.acme.center.platform.iam.domain.services;
+
+import com.acme.center.platform.iam.domain.model.aggregates.User;
+import com.acme.center.platform.iam.domain.model.commaands.SignInCommand;
+import com.acme.center.platform.iam.domain.model.commaands.SignUpCommand;
+import com.acme.center.platform.iam.domain.model.queries.GetUserByIdQuery;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.Optional;
+
+public interface UserCommandService {
+
+    Optional<ImmutablePair<User, String>> handle(SignInCommand command);
+
+    Optional<User> handle(SignUpCommand command);
+}
