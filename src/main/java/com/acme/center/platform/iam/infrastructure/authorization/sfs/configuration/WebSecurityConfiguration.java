f1897d37ba0a4873dbd59981970670bb7b8b69ca
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-26 22:49:54 -0500

    feat(iam): configuring sprint security

---
+++ src/main/java/com/acme/center/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,96 @@
+package com.acme.center.platform.iam.infrastructure.authorization.sfs.configuration;
+
+import com.acme.center.platform.iam.infrastructure.hashing.bcrypt.BCryptHashingService;
+import com.acme.center.platform.iam.infrastructure.tokens.jwt.BearerTokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfiguration {
+
+    private final UserDetailsService userDetailsService;
+
+    private final BearerTokenService tokenService;
+
+    private final BCryptHashingService hashingService;
+
+    private final AuthenticationEntryPoint unauthorizedRequestHandler;
+
+    public WebSecurityConfiguration(UserDetailsService userDetailsService, BearerTokenService tokenService, BCryptHashingService hashingService, AuthenticationEntryPoint unauthorizedRequestHandler) {
+        this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
+        this.hashingService = hashingService;
+        this.unauthorizedRequestHandler = unauthorizedRequestHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return hashingService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+//    @Bean
+//    public BearerAuthorizationRequestFilter authorizationRequestFilter() {
+//        return new BearerAuthorizationRequestFilter(tokenService, userDetailsService);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors(c -> c.configurationSource( _ -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("*"));
+            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+        }));
+
+        http.csrf(c -> c.disable());
+
+        http.exceptionHandling(exHandling -> exHandling.authenticationEntryPoint(unauthorizedRequestHandler))
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(
+                                "/api/v1/authentication/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "webjars/**"
+                        ).permitAll().anyRequest().authenticated());
+
+        http.authenticationProvider(authenticationProvider());
+        //http.addFilterBefore(authorizationRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
